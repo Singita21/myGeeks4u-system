925862a3b5c4d03101df4c6913c8544259722687
unknown <Singitaangel17@gmail.com>
2023-09-15 15:49:32 +0200

    Project Update

---
+++ app/src/main/java/com/example/mygeeks4u/activities/SignUpActivity.java
@@ -0,0 +1,14 @@
+package com.example.mygeeks4u.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.mygeeks4u.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
---
+++ app/src/main/java/com/example/mygeeks4u/activities/signInActivity.java
@@ -0,0 +1,16 @@
+package com.example.mygeeks4u.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.mygeeks4u.R;
+
+public class signInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
