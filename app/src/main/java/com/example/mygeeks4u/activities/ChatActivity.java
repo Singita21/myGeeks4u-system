8fe7c3670e696e718456570ce44d5faf1abc0091
User <Singitangel17@gmail.com>
2023-11-29 15:25:30 +0200

    Initialise commit

--- app/src/main/java/com/example/mygeeks4u/activities/ChatActivity.java
+++ app/src/main/java/com/example/mygeeks4u/activities/ChatActivity.java
@@ -1,40 +1,50 @@
 package com.example.mygeeks4u.activities;
 
-import static java.util.Base64.*;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
+import android.provider.MediaStore;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Base64;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
-import com.example.mygeeks4u.R;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.mygeeks4u.adapters.ChatAdapter;
 import com.example.mygeeks4u.databinding.ActivityChatBinding;
 import com.example.mygeeks4u.models.ChatMessage;
 import com.example.mygeeks4u.models.User;
 import com.example.mygeeks4u.utilities.Constants;
 import com.example.mygeeks4u.utilities.PreferenceManager;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
-
-
 public class ChatActivity extends AppCompatActivity {
 
     private ActivityChatBinding binding;
@@ -49,30 +59,42 @@ public class ChatActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         binding = ActivityChatBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setListeners();
+
         loadReceiverDetails();
         init();
-        listenMessages();
+        listenMessage();
+
+
 
     }
 
     private void init()
     {
-
-        if (receiverUser != null && receiverUser.image != null) {
-            chatAdapter = new ChatAdapter(
-                    chatMessages,
-                    getBitmapFromEncodedString(receiverUser.image),
-                    preferenceManager.getString(Constants.KEY_USER_ID)
-            );
-            binding.chatRecyclerView.setAdapter(chatAdapter);
-        }
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        chatMessageList = new ArrayList<>();
+        chatAdapter = new ChatAdapter(
+                chatMessageList,getBitmapFromEncodedString(receiverUser.image),preferenceManager.getString(Constants.KEY_USER_ID)
+        );
+        binding.chatRecyclerView.setAdapter(chatAdapter);
         database = FirebaseFirestore.getInstance();
 
-
     }
+
+    private Bitmap getBitmapFromEncodedString(String encodedImage)
+    {
+        byte[] bytes = Base64.decode(encodedImage,Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+    }
+
+
+
+
     private void sendMessage()
     {
         HashMap<String, Object> message = new HashMap<>();
@@ -83,66 +105,91 @@ public class ChatActivity extends AppCompatActivity {
         database.collection(Constants.KEY_COLLECTION_CHAT).add(message);
         binding.inputMessage.setText(null);
     }
-    private void listenMessages(){
+
+
+
+
+    public void listenMessage()
+    {
         database.collection(Constants.KEY_COLLECTION_CHAT)
                 .whereEqualTo(Constants.KEY_SENDER_ID, preferenceManager.getString(Constants.KEY_USER_ID))
                 .whereEqualTo(Constants.KEY_RECEIVER_ID, receiverUser.id)
                 .addSnapshotListener(eventListener);
+
         database.collection(Constants.KEY_COLLECTION_CHAT)
                 .whereEqualTo(Constants.KEY_SENDER_ID, receiverUser.id)
                 .whereEqualTo(Constants.KEY_RECEIVER_ID, preferenceManager.getString(Constants.KEY_USER_ID))
                 .addSnapshotListener(eventListener);
-
     }
+
     private final EventListener<QuerySnapshot> eventListener = (value, error) -> {
-        if(error != null){
+        if (error != null) {
+            // Handle the error
             return;
         }
-        if(value != null) {
-            int count = chatMessages.size();
+        if (value != null) {
+            int count = chatMessageList.size();
             for (DocumentChange documentChange : value.getDocumentChanges()) {
                 if (documentChange.getType() == DocumentChange.Type.ADDED) {
                     ChatMessage chatMessage = new ChatMessage();
                     chatMessage.senderId = documentChange.getDocument().getString(Constants.KEY_SENDER_ID);
                     chatMessage.receiverId = documentChange.getDocument().getString(Constants.KEY_RECEIVER_ID);
                     chatMessage.message = documentChange.getDocument().getString(Constants.KEY_MESSAGE);
-                    chatMessage.dateTime = getReadableDataTime(documentChange.getDocument().getDate(Constants.KEY_TIMESTAMP));
+                    chatMessage.dateTime = getReadableDate(documentChange.getDocument().getDate(Constants.KEY_TIMESTAMP));
                     chatMessage.dateObject = documentChange.getDocument().getDate(Constants.KEY_TIMESTAMP);
-                    chatMessages.add(chatMessage);
+
+                    chatMessageList.add(chatMessage);
                 }
             }
-            Collections.sort(chatMessages, (obj1, obj2) -> obj1.dateObject.compareTo(obj2.dateObject));
-            if (count == 0) {
+            Collections.sort(chatMessageList, (obj1, obj2) -> obj1.dateObject.compareTo(obj2.dateObject));
+            if(count == 0)
+            {
                 chatAdapter.notifyDataSetChanged();
-            } else {
-                chatAdapter.notifyItemRangeInserted(chatMessages.size(), chatMessages.size());
-                binding.chatRecyclerView.smoothScrollToPosition(chatMessages.size() - 1);
-
+            }
+            else
+            {
+                chatAdapter.notifyItemRangeInserted(chatMessageList.size(),chatMessageList.size());
+                binding.chatRecyclerView.smoothScrollToPosition(chatMessageList.size() - 1);
             }
             binding.chatRecyclerView.setVisibility(View.VISIBLE);
         }
         binding.progressBar.setVisibility(View.GONE);
-    };
-    private Bitmap getBitmapFromEncodedString(String encodedImage)
-    {
-        byte[] bytes = Base64.decode(encodedImage,Base64.DEFAULT);
-        return BitmapFactory.decodeByteArray(bytes,0,bytes.length);
-    }
-    private Bitmap getUserImage(String encodedImage)
-    {
-        byte[] bytes = Base64.decode(encodedImage,Base64.DEFAULT);
-        return BitmapFactory.decodeByteArray(bytes,0,bytes.length);
-    }
 
-    private void loadReceiverDetails(){
+    };
+    private void loadReceiverDetails()
+    {
         receiverUser = (User) getIntent().getSerializableExtra(Constants.KEY_USER);
+
         binding.textName.setText(receiverUser.name);
     }
-    private void setListeners() {
-        binding.imageBack.setOnClickListener(v -> onBackPressed());
+
+    private void setListeners()
+    {
+        binding.imageBack.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(),UsersActivity.class)));
         binding.layoutSend.setOnClickListener(v -> sendMessage());
+
     }
-    private String getReadableDataTime(Date date){
-        return new SimpleDateFormat("MMMM dd, yyyy - hh:mm a", Locale.getDefault()).format(date);
+
+
+    private String getReadableDate (Date date){
+
+        long currentTimeMillis = System.currentTimeMillis();
+        long timeDifferenceMillis = currentTimeMillis - date.getTime();
+
+        if(timeDifferenceMillis <= 24 * 60 * 60 * 1000)
+        {
+            return "Today, "+ new SimpleDateFormat("hh:mm a", Locale.getDefault()).format(date);
+        }
+        else if(timeDifferenceMillis > 24 * 60 * 60 * 1000 && timeDifferenceMillis <= 48 * 60 * 60 * 1000)
+        {
+            return "Yesterday, " + new SimpleDateFormat("hh:mm a", Locale.getDefault()).format(date);
+        }
+        else
+        {
+            return new SimpleDateFormat("MMMM dd, - hh:mm a", Locale.getDefault()).format(date);
+        }
+
     }
+
 }
--- app/src/main/java/com/example/mygeeks4u/activities/UsersActivity.java
+++ app/src/main/java/com/example/mygeeks4u/activities/UsersActivity.java
@@ -73,6 +73,7 @@ public class UsersActivity extends AppCompatActivity implements UserListener {
                     user.name = queryDocumentSnapshot.getString(Constants.KEY_NAME);
                     user.email = queryDocumentSnapshot.getString(Constants.KEY_EMAIL);
                     user.token = queryDocumentSnapshot.getString(Constants.KEY_FCM_TOKEN);
+                    user.image = queryDocumentSnapshot.getString(Constants.KEY_IMAGE);
                     user.id = queryDocumentSnapshot.getId();
                     users.add(user);
 
